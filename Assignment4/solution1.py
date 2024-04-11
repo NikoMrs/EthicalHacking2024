4b256c4cb341c69b00e9fea269fee8ee5ddc2ddb
NikoMrs <mores.nicola@gmail.com>
2024-04-11 14:56:51 +0200

    refactor comments

--- Assignment4/solution1.py
+++ Assignment4/solution1.py
@@ -6,14 +6,14 @@ sys.set_int_max_str_digits(50000)
 REMOTE = True
 
 IP_ADDRESS = "cyberchallenge.disi.unitn.it"
-PORT = 50303
+PORT = 50302
 
 if(REMOTE):
     conn = pwn.remote(IP_ADDRESS, PORT)
 else:
-    conn = pwn.process(['python3', 'challenge2.py'])
+    conn = pwn.process(['python3', 'challenge1.py'])
 
-# Retrieve the encrypted Flag c = t^e mod n
+# Retrieve the encrypted Flag c = s^e mod n
 conn.recvuntil(b":")
 flag = int((conn.recvline(keepends=False)).decode())
 print("Encrypted Flag (c): ", flag)
@@ -21,38 +21,38 @@ c = flag
 
 # Set parameters used for the malleability attack
 e = 65537
-g = 2
+betha = 2
 
-# Get c_a = g^e mod n
+# Get alpha = betha^e mod n
 conn.recvuntil(b"> ")
 conn.sendline(b"1")
 
 conn.recvuntil(b"> ")
-conn.sendline(long_to_bytes(g))
+conn.sendline(long_to_bytes(betha))
 
 conn.recvuntil(b":" )
-c_a = int(conn.recvline(keepends=False).decode())
+alpha = int(conn.recvline(keepends=False).decode())
 
-print("Ca: ", c_a)
+print("Alpha: ", alpha)
 
-# Compute c_b = c * c_a
-c_b = c * c_a
-print("Cb: ", c_b)
+# Compute C = c * alpha
+C = c * alpha
+print("C: ", C)
 
-# Send c_b to the server
+# Send C to the server
 conn.recvuntil(b"> ")
 conn.sendline(b"2")
 
 conn.recvuntil(b"> ")
-conn.sendline(str(c_b).encode())
+conn.sendline(str(C).encode())
 
-# Get m from the server. m = c_b^d mod n = (c * c_a)^d mod n = t * g
+# Get m from the server. m = C^d mod n = (c * alpha)^d mod n = s * betha
 conn.recvuntil(b": ")
 m = int(conn.recvline(keepends=False).decode())
 
 print("M: ", m)
 
-# Compute t from m
-t = m // g
-print("T: ", t)
-print("Flag: ", long_to_bytes(t).decode())
+# Compute s from m
+s = m // betha
+print("S: ", s)
+print("Flag: ", long_to_bytes(s).decode())
