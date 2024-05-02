92b966bd7ebac33b7aa3ec1bcae6e006a1e8d92d
NikoMrs <mores.nicola@gmail.com>
2024-05-02 19:21:51 +0200

    init Lecture6

---
+++ Lecture6/es3.py
@@ -0,0 +1,6 @@
+from z3 import *
+
+format = "UniTN{bXXXX_XX_XX_XXXXXr_XXXXXXX}"
+flag = [None]*21
+print(flag)
+
---
+++ Lecture6/es4.py
@@ -0,0 +1,39 @@
+import pwn
+
+from ctypes import CDLL
+libc = CDLL("libc.so.6")
+
+from Crypto.Util.number import long_to_bytes, bytes_to_long
+
+
+REMOTE = True
+
+IP_ADDRESS = "cyberchallenge.disi.unitn.it"
+PORT = 8103
+
+if(REMOTE):
+    conn = pwn.remote(IP_ADDRESS, PORT)
+else:
+    conn = pwn.process(['python3', 'challenge1.py'])
+
+currentTime = libc.time(0)
+
+libc.srand(currentTime)
+
+psw = [None]*16
+for i in range(len(psw)):
+    randNum = libc.rand()
+    psw[i] = randNum % 0x5e + ord('!')
+
+print(psw)
+
+fullPsw = ""
+for i in range(len(psw)):
+    fullPsw = fullPsw + chr(psw[i])
+
+print(fullPsw)
+
+conn.recvuntil(b":")
+conn.sendline(fullPsw.encode())
+
+conn.interactive()
