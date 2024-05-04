a5649aaf66f1721152e81f2a120046d87f822c83
NikoMrs <mores.nicola@gmail.com>
2024-05-04 19:29:36 +0200

    refactor comments

--- Assignment6/solution.py
+++ Assignment6/solution.py
@@ -21,30 +21,28 @@ conn.recvuntil(b": ")
 firstPsw = conn.recvline(keepends=False).decode()
 print(f"First Password: {firstPsw}")
 
-index = 0
 for i in range(32768):              # Iterate from 0 to (Default) Max PID possibile in Linux
 
-    print(f"Tentativo {i}")
+    print(f"Try number {i}")
     libc.srand(i)                   # Initiate srand with seed equal to selected PID
 
-    myPsw = ""                      # Create psw following disassembled code
+    myPsw = ""                      # Create psw in the same way as rand_pass()
     for j in range(16):
         randNum = libc.rand()
         myPsw += chr(randNum % 0x5e + ord('!'))
 
     print(myPsw)
 
-    if(myPsw == firstPsw):          # Check if myPsw is equal to the one received
-        index = i
+    if(myPsw == firstPsw):          # Check if myPsw is equal to the received one
         break
     
-secondPsw = ""                      # Create another psw using the same seed
+guessedPsw = ""                     # Create another psw using the same seed
 for i in range(16):
     randNum = libc.rand()
-    secondPsw += chr(randNum % 0x5e + ord('!'))
+    guessedPsw += chr(randNum % 0x5e + ord('!'))
     
-conn.recvuntil(b":")                # Send second psw and retrieve the flag
-conn.sendline(secondPsw.encode())
+conn.recvuntil(b":")                # Send our guessed psw and retrieve the flag
+conn.sendline(guessedPsw.encode())
 
 conn.recvuntil(b":\n")
 flag = (conn.recvline(keepends=False)).decode()
